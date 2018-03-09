a0b835397697e721f1a66a4e5085117c4208cce9
TerezaCAI <37206489+TerezaCAI@users.noreply.github.com>
2018-03-09 17:15:28 +0800

    Add files via upload

---
+++ homework2/CAIJiajing.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re 
+
+hacker='https://news.ycombinator.com/'
+
+title=[]
+score=[]
+time=[]
+for i in range(1,100):
+	url=hacker+'news?p='+str(i)
+	r=requests.get(url).text
+	page=BeautifulSoup(r,'html.parser')
+	area=page.find_all('a',attrs={'class':'storylink'})
+	for t in area:
+		title.append(t.text)
+	subtext=page.find_all('span',attrs={'class':'score'})
+	for s in subtext:
+		score.append(s.text)
+	age=page.find_all('span',attrs={'class':'age'})
+	for a in age:
+		time.append(a.text)
+with open('hw2.csv','w') as f:
+	writer=csv.writer(f)
+	header=['title','score','time']
+	writer.writerow(header)
+	rows=zip(title,score,time)
+	for row in rows:
+		writer.writerow(row)
+
